583fa4c2fca087d9c89025d9d028bbdb7b3533fd
Iokin <vvhappyguy@gmail.com>
2017-04-21 21:06:15 +0300

    Starting Including Bootstrap 3

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+  <head>
+    <title>Шаблон Bootstrap 101</title>
+	<meta charset="utf-8">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+  </head>
+  <body>
+    <h1>Привет, мир!</h1>
+    <script src="http://code.jquery.com/jquery-latest.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+  </body>
+</html>
